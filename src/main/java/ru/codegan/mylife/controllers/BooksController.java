a68a520d5af24e0aeb3c9384e46e6abfa0d05f80
codegan <elbukaevzaur@gmail.com>
2017-12-12 16:41:13 +0300

    adding spring security successfully

---
+++ src/main/java/ru/codegan/mylife/controllers/BooksController.java
@@ -0,0 +1,85 @@
+package ru.codegan.mylife.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.codegan.mylife.model.Books;
+import ru.codegan.mylife.model.BooksUsed;
+import ru.codegan.mylife.model.BooksUsedStatus;
+import ru.codegan.mylife.services.BooksService;
+
+@RestController
+@RequestMapping("/books")
+@Secured(value = {"ROLE_ADMIN"})
+public class BooksController {
+	
+	private BooksService booksService;
+	
+	@Autowired
+	public void setBooksService(BooksService booksService){
+		this.booksService = booksService;
+	}
+	//Books
+	@RequestMapping(method = RequestMethod.GET, value="/")
+	public @ResponseBody List<Books> findBooks() {
+		return this.booksService.findAllBooks();	
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value="/")
+	public List<Books> addBooks(@RequestBody Books books) {
+		this.booksService.saveBooks(books);
+		return this.findBooks();
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value="/")
+	public List<Books> updateBooks(@RequestBody Books books) {
+		this.booksService.saveBooks(books);
+		return this.findBooks();
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value="/{id}")
+	public List<Books> removeBooks(@PathVariable int id) {
+		Books books = new Books();
+		books.setId(id);
+		this.booksService.removeBooks(books);
+		return this.findBooks();
+	}
+	//Books_Used
+	@RequestMapping(method = RequestMethod.GET, value="/used")
+	public @ResponseBody List<BooksUsed> findBooksUsed() {
+		return this.booksService.findAllBooksUsed();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value="/used")
+	public List<BooksUsed> addBooksUsed(@RequestBody BooksUsed booksUsed) {
+		this.booksService.saveBooksUsed(booksUsed);
+		return this.booksService.findAllBooksUsed();
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value="/used")
+	public List<BooksUsed> updateBooksUsed(@RequestBody BooksUsed booksUsed) {
+		this.booksService.saveBooksUsed(booksUsed);
+		return this.booksService.findAllBooksUsed();
+	}
+	@RequestMapping(method = RequestMethod.DELETE, value="/used")
+	public List<BooksUsed> removeBooksUsed(@RequestBody BooksUsed booksUsed) {
+		this.booksService.removeBooksUsed(booksUsed);
+		return this.booksService.findAllBooksUsed();
+	}
+	//Used
+	@RequestMapping(method = RequestMethod.GET, value="/used/status/{id}")
+	public @ResponseBody List<BooksUsed> getUsedStatusBooks(@PathVariable("id") int used_status_id) {
+		return this.booksService.findAllStatusBooks(used_status_id);
+	}
+	
+	
+}
